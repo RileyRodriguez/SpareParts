1190a45a43de7bf060a9e5d77c0aab86074adb0e
Riley Rodriguez <riley.rodriguez.c@gmail.com>
2023-03-21 13:23:26 -0400

    PlayerAnimation
    Rebuilt player object with new controller and animator.  Player animates correctly.

---
+++ SpareParts/Assets/SimpleCharacterController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    public Animator animator;
+    private CharacterController characterController;
+    public float Speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Compute wasd axes / movement
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        float isMoving = Mathf.Abs(Horizontal) + Mathf.Abs(Vertical);
+
+        //Update Animations
+        animator.SetFloat("isMoving", isMoving);
+        animator.SetFloat("Horizontal", Horizontal);
+        animator.SetFloat("Vertical", Vertical);
+
+        //Move Character
+        Vector3 move = new Vector3(Horizontal, 0, Vertical);
+        characterController.Move(move * Time.deltaTime * Speed);
+
+        
+    }
+}
